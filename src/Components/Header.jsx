7c7586a7493e8feeb0d9799edc42f718abff3188
dpc44 <caophat130896@gmail.com>
2023-06-22 21:24:04 +0700

    header-fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
-import React, { Component,useRef,useState,useEffect } from 'react'
-import {  connect, useDispatch, useSelector, } from 'react-redux'
-import { NavLink,useSearchParams,  } from 'react-router-dom'
+import React, { Component, useRef, useState, useEffect } from 'react'
+import { connect, useDispatch, useSelector, } from 'react-redux'
+import { NavLink, useSearchParams, } from 'react-router-dom'
 import { USER_SHOE } from '../util/config'
 import { loginAction } from '../redux/reducers/UserReducer'
 import { customNavigate } from '..'
-const Header =(props)=> {
+const Header = (props) => {
     const { userLogin } = useSelector(state => state.UserReducer);
-    const [searchParams,setSearchParams] = useSearchParams();
-    const keywordRef =useRef()
+    const [searchParams, setSearchParams] = useSearchParams();
+    const keywordRef = useRef()
     const key = searchParams.get('keyword');
     // const searchString= useRef();
     const dispatch = useDispatch();
@@ -32,30 +32,35 @@ const Header =(props)=> {
                 </li>
             </>
         }
-        return <li className="nav-item">
-            <NavLink className="nav-link" to="/login">Login</NavLink>
-        </li>
+        return <>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/register">Register</NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" to="/login">Login</NavLink>
+            </li>
+        </>
     }
-   const handleChange=(e)=>{
+    const handleChange = (e) => {
         const { value } = e.target;
-        keywordRef.current =value;
-    
-        
+        keywordRef.current = value;
+
+
     }
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
         setSearchParams({
-            keyword:keywordRef.current
+            keyword: keywordRef.current
         })
         let action = {
-            type:'SEARCH_VALUE',          
-            keyword:keywordRef.current
+            type: 'SEARCH_VALUE',
+            keyword: keywordRef.current
         }
         dispatch(action)
         document.getElementById('frm').reset();
     }
-    return (  
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <NavLink className="navbar-brand" to="/">Cybersoft Shop</NavLink>
             <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
             <div className="collapse navbar-collapse" id="collapsibleNavId">
@@ -64,21 +69,22 @@ const Header =(props)=> {
                         <NavLink className="nav-link active" to="/" aria-current="page">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/register">Register</NavLink>
-                    </li>
-                    {renderLogin()}
-                    <li className="nav-item">
                         <NavLink className="nav-link" to="/search">Search</NavLink>
                     </li>
+                    <form className="d-flex my-2 my-lg-0 navbar-form navbar-left" id='frm'>
+                        <input className="form-control me-sm-2" type="text" placeholder="Search" id='keyword' onChange={handleChange} />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={handleSubmit}>Search</button>
+                    </form>
                 </ul>
-                <form className="d-flex my-2 my-lg-0" id='frm'>
-                    <input className="form-control me-sm-2" type="text" placeholder="Search"  id='keyword' onChange={handleChange}/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={handleSubmit}>Search</button>
-                </form>
+
+
+                <ul className="navbar-nav mt-2 mt-lg-0">
+                    {renderLogin()}
+                </ul>
             </div>
         </nav>
     )
-  }
+}
 const mapStateToProps = (state) => ({})
 export default connect(mapStateToProps)(Header)
 
