774c2fe8e1aebab9f93c1a67642407abdca9cace
dpc44 <caophat130896@gmail.com>
2023-07-02 01:32:28 +0700

    CSS-header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,7 +18,6 @@ const Header = (props) => {
             return <>
                 <li className="nav-item">
                     <NavLink className="nav-link" to="/cart">{gioHang.length === 0 ? <i class="fa fa-shopping-cart" aria-hidden="true"></i> : <div><span>({gioHang.length})</span><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>}</NavLink>
-                    <NavLink className="nav-link" to="/cart">{gioHang.length === 0 ? <i class="fa fa-shopping-cart" aria-hidden="true"></i> : <div><span>({gioHang.length})</span><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>}</NavLink>
                 </li>
                 <li className="nav-item">
                     <NavLink className="nav-link" to="/profile">
@@ -46,52 +45,46 @@ const Header = (props) => {
             </li>
         </>
     }
-const handleChange = (e) => {
-    const { value } = e.target;
-    keywordRef.current = value;
-}
-const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearchParams({
-        keyword: keywordRef.current
-    })
-    let action = searchAction(
-        keywordRef.current
-    );
-    dispatch(action)
-    document.getElementById('frm').reset();
+    const handleChange = (e) => {
+        const { value } = e.target;
+        keywordRef.current = value;
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchParams({
+            keyword: keywordRef.current
+        })
+        let action = searchAction(
+            keywordRef.current
+        );
+        dispatch(action)
+        document.getElementById('frm').reset();
 
-}
-return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <NavLink className="navbar-brand" to="/">Cybersoft Shop</NavLink>
-        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-                <li className="nav-item">
-                    <NavLink className="nav-link active" to="/" aria-current="page">Home</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/search">Search</NavLink>
-                </li>
-                <form className="d-flex my-2 my-lg-0 navbar-form navbar-left" id='frm'>
-                    <input className="form-control me-sm-2" type="text" placeholder="Search" id='keyword' onChange={handleChange} />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={handleSubmit}>Search</button>
-                </form>
-            </ul>
+    }
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <NavLink className="navbar-brand" to="/">Cybersoft Shop</NavLink>
+            <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
+            <div className="collapse navbar-collapse" id="collapsibleNavId">
+                <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li className="nav-item">
+                        <NavLink className="nav-link active" to="/" aria-current="page">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/search">Search</NavLink>
+                    </li>
+                    <form className="d-flex my-2 my-lg-0 navbar-form navbar-left" id='frm'>
+                        <input className="form-control me-sm-2" type="text" placeholder="Search" id='keyword' onChange={handleChange} />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={handleSubmit}>Search</button>
+                    </form>
+                </ul>
 
-            <ul className="navbar-nav mt-2 mt-lg-0">
-                {renderLogin()}
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/profile">Profile</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/register">Register</NavLink>
-                </li>
-            </ul>
-        </div>
-    </nav>
-)
+                <ul className="navbar-nav mt-2 mt-lg-0">
+                    {renderLogin()}
+                </ul>
+            </div>
+        </nav>
+    )
 }
 const mapStateToProps = (state) => ({})
 export default connect(mapStateToProps)(Header)
